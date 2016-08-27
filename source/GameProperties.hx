fd794b2ecf021dfd02424f30f3920624aaa13693
Thunraz <julian.dinges@gmail.com>
2016-08-27 11:16:50 +0200

    Added GameProperties

---
+++ source/GameProperties.hx
@@ -0,0 +1,10 @@
+package;
+
+import flixel.math.FlxPoint;
+
+class GameProperties
+{
+    public static var PlayerMovementAcceleration: Float = 750;
+    public static var PlayerMovementDrag: FlxPoint = new FlxPoint(2500, 2500);
+    public static var PlayerMovementMaxVelocity: FlxPoint = new FlxPoint(200, 200);
+}
