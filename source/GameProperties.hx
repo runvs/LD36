dafd4f26b732a27f66944d8067d5cb1a2749da9d
Thunraz <julian.dinges@gmail.com>
2016-08-27 11:36:45 +0200

    Changed _dashWallTime to _dashCooldown and moved it to GameProperties

--- source/GameProperties.hx
+++ source/GameProperties.hx
@@ -4,7 +4,9 @@ import flixel.math.FlxPoint;
 
 class GameProperties
 {
-    public static var PlayerMovementAcceleration: Float = 750;
-    public static var PlayerMovementDrag: FlxPoint = new FlxPoint(2500, 2500);
-    public static var PlayerMovementMaxVelocity: FlxPoint = new FlxPoint(200, 200);
+    // ################################################################
+    public static var PlayerMovementAcceleration : Float = 750.0;
+    public static var PlayerMovementDrag         : FlxPoint = new FlxPoint(2500, 2500);
+    public static var PlayerMovementMaxVelocity  : FlxPoint = new FlxPoint(200, 200);
+    public static var PlayerMovementDashCooldown : Float = 1.0;
 }
