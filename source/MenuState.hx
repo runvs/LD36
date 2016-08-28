9cd91ca50f104b54323a8c7d672fcab47badd96b
Simon Weis <laguna.ws@gmail.com>
2016-08-28 16:33:28 +0200

    pre pull commit

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,10 +9,17 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
+	var title : FlxText;
+	var t_play : FlxText;
+	var t_credits : FlxText;
+	
+	
 	override public function create():Void
 	{
 		super.create();
 		FlxG.mouse.visible = false;
+		
+		title = ne
 	}
 
 	override public function update(elapsed:Float):Void
